9cc49dc7c91d7a3d7e8739d5342cbe01b7e59667
curtismchale <curtis@curtismchale.ca>
2023-10-07 17:27:07 -0700

    plugin header

---
+++ wp-book-tracking.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Book Tracker
+* Plugin URI: https://curtismchale.ca/
+* Description: Lets you track your reading and your library
+* Version: 0.1
+* Author: curtismchale
+* Author URI: https://curtismchale.ca/
+**/
