1e103008c24c133cb94ca62bf0c1173195c3aae0
curtismchale <curtis@curtismchale.c>
2023-10-23 21:04:15 -0700

    establishing the base plugin version

--- wp-book-tracking.php
+++ wp-book-tracking.php
@@ -7,3 +7,113 @@
 * Author: curtismchale
 * Author URI: https://curtismchale.ca/
 **/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+class Book_Tracking{
+
+	private static $instance;
+
+	/**
+	 * Spins up the instance of the plugin so that we don't get many instances running at once
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 *
+	 * @uses $instance->init()                      The main get it running function
+	 */
+	public static function instance(){
+
+		if ( ! self::$instance ){
+			self::$instance = new Book_Tracking();
+			self::$instance->init();
+		}
+
+	} // instance
+
+	/**
+	 * Spins up all the actions/filters in the plugin to really get the engine running
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 *
+	 * @uses $this->constants()                 Defines our constants
+	 * @uses $this->includes()                  Gets any includes we have
+	 */
+	public function init(){
+
+		$this->constants();
+		$this->includes();
+
+		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall' ) );
+
+	} // init
+
+	/**
+	 * Gives us any constants we need in the plugin
+	 *
+	 * @since 1.0
+	 */
+	public function constants(){
+
+		define( 'BOOK_TRACK_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+	}
+
+	/**
+	 * Includes any externals
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 * @access public
+	 */
+	public function includes(){
+
+	}
+
+	/**
+	 * Fired when plugin is activated
+	 *
+	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+	 */
+	public function activate( $network_wide ){
+
+	} // activate
+
+	/**
+	 * Fired when plugin is deactivated
+	 *
+	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+	 */
+	public function deactivate( $network_wide ){
+
+	} // deactivate
+
+	/**
+	 * Fired when plugin is uninstalled
+	 *
+	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+	 */
+	public function uninstall( $network_wide ){
+
+	} // uninstall
+
+} // Book_Tracking
